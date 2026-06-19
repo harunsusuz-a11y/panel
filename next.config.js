fe9667c1f11b55b9364ea38f47fafdad8ee1d1cd
harunsusuz-a11y <harunsusuz@daydreamproduction.com.tr>
2026-06-19 14:13:22 +0300

    fix: next 15.3.6 official CVE patch

--- next.config.js
+++ next.config.js
@@ -1,8 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'standalone',
-  env: {
-    NEXT_TELEMETRY_DISABLED: '1'
-  }
-}
+const nextConfig = {}
 module.exports = nextConfig
