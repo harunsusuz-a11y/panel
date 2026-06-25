495e784cb95e14e640137efae20de72a634eb87b
harunsusuz-a11y <harunsusuz@daydreamproduction.com.tr>
2026-06-25 12:31:04 +0300

    chore: force redeploy with cache bust

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  // updated: '2026-06-25'
+}
 module.exports = nextConfig
+
